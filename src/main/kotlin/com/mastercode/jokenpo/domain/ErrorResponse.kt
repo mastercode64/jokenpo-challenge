6a7cd89c9a98363af56b0ec544209d3a595e148b
Marcos Fujihashi <marcosfujihashi@gmail.com>
2026-02-18 11:38:01 -0300

    Added custom error response

---
+++ src/main/kotlin/com/mastercode/jokenpo/domain/ErrorResponse.kt
@@ -0,0 +1,5 @@
+package com.mastercode.jokenpo.domain
+
+data class ErrorResponse(
+    val message: String
+)
---
+++ src/main/kotlin/com/mastercode/jokenpo/exceptionhandler/GlobalExceptionHandler.kt
@@ -0,0 +1,25 @@
+package com.mastercode.jokenpo.exceptionhandler
+
+import com.mastercode.jokenpo.domain.Move
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotReadableException
+import org.springframework.web.ErrorResponse
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+
+@ControllerAdvice
+class GlobalExceptionHandler {
+
+    @ExceptionHandler(HttpMessageNotReadableException::class)
+    fun handleInvalidEnum(ex: HttpMessageNotReadableException): ResponseEntity<com.mastercode.jokenpo.domain.ErrorResponse> {
+
+        val validMoves = Move.entries.joinToString(", ")
+
+        val message = "Invalid move. Valid options are: $validMoves."
+
+        return ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(com.mastercode.jokenpo.domain.ErrorResponse(message))
+    }
+}
