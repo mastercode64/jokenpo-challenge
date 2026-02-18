e320fd6f2e87da50d0f0c244ac0cf8361bd766d9
Marcos Fujihashi <marcosfujihashi@gmail.com>
2026-02-18 11:54:13 -0300

    remove import

--- src/main/kotlin/com/mastercode/jokenpo/exceptionhandler/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/mastercode/jokenpo/exceptionhandler/GlobalExceptionHandler.kt
@@ -4,7 +4,6 @@ import com.mastercode.jokenpo.domain.Move
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.http.converter.HttpMessageNotReadableException
-import org.springframework.web.ErrorResponse
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 
