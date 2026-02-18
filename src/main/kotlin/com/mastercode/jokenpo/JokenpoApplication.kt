09cbe247bcf26ba68e48c1cea373079c1817f30b
Marcos Fujihashi <marcosfujihashi@gmail.com>
2026-02-18 10:53:59 -0300

    added initial project files

---
+++ src/main/kotlin/com/mastercode/jokenpo/JokenpoApplication.kt
@@ -0,0 +1,11 @@
+package com.mastercode.jokenpo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JokenpoApplication
+
+fun main(args: Array<String>) {
+	runApplication<JokenpoApplication>(*args)
+}
---
+++ src/test/kotlin/com/mastercode/jokenpo/JokenpoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mastercode.jokenpo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JokenpoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
